141b0975f5ba3b1db38855d76cc0ad0d1b96d063
rickspears <valiryonh@yahoo.com>
2015-03-10 20:15:06 -0700

    Rename ConsignmentManager project to ConsignmentManager.Web

--- ConsignmentManager.Web.Tests/Properties/AssemblyInfo.cs
+++ ConsignmentManager.Web.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsignmentManager.Tests")]
+[assembly: AssemblyTitle("ConsignmentManager.Web.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsignmentManager.Tests")]
+[assembly: AssemblyProduct("ConsignmentManager.Web.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7cfaf734-228f-4ee6-9320-a1694ee9efbd")]
+[assembly: Guid("6808dbd5-ac24-4d4d-ae0d-0bfd55cf9f09")]
 
 // Version information for an assembly consists of the following four values:
 //
