864b3409f6d7e6a179f546732691e2e6ed8692d1
rickspears <valiryonh@yahoo.com>
2015-03-10 20:21:22 -0700

    Add Web API Controller
    Add Web API Controller
    - Add some overlooked items due to .Web project rename in previous
    commit
    - Add an initial api route that gets all items in db

---
+++ ConsignmentManager.Web/Controllers/JBFController.cs
@@ -0,0 +1,25 @@
+﻿using ConsignmentManager.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Remoting.Contexts;
+using System.Web.Http;
+
+namespace ConsignmentManager.Web.Controllers
+{
+    public class JBFController : ApiController
+    {
+      private CMContext db = new CMContext();
+
+      // GET: api/analytics
+      [Route("api/analytics")]
+      public HttpResponseMessage Get()
+      {
+        var items = db.Items;
+        HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, items);
+        return response;
+      }
+    }
+}
