2f8f7286f5c90256be7ea8e11044bf7270e94177
rickspears <valiryonh@yahoo.com>
2015-03-29 16:38:46 -0700

    Fix dates being off by one day
    - Javascript Date class was applying the local time zone to a date that
    was already generated in local time
    - Fix is only for Pacific time

--- ConsignmentManager.Web/Scripts/app/services/services.js
+++ ConsignmentManager.Web/Scripts/app/services/services.js
@@ -8,9 +8,34 @@
 
   function cmService() {
     function getYearMonthDay(dateString) {
-      var date = new Date(dateString);
+      var date = new Date(dateString).restoreTimeZoneOffset();
       var ymd = date.toLocaleDateString();
       return ymd;
+    }
+
+    Date.prototype.restoreTimeZoneOffset = function () {
+      if (this.dst()) {
+        this.addHours(7);
+      }
+      else {
+        this.addHours(8);
+      }
+      return this;
+    }
+
+    Date.prototype.addHours = function(hours){
+      this.setHours(this.getHours()+hours);
+      return this;
+    }
+    
+    Date.prototype.stdTimezoneOffset = function() {
+      var jan = new Date(this.getFullYear(), 0, 1);
+      var jul = new Date(this.getFullYear(), 6, 1);
+      return Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
+    }
+    
+    Date.prototype.dst = function () {
+      return this.getTimezoneOffset() < this.stdTimezoneOffset();
     }
 
     return {
